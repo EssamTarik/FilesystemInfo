792600d6f004a548ac55fd032665be15f9aa2da0
EssamTarik <essamtarik2000@gmail.com>
2017-05-05 19:17:22 +0200

    project html interface

--- main.pl
+++ main.pl
@@ -1,3 +1,9 @@
+:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_error)).
+:- use_module(library(http/http_parameters)).
+
+
 bash_command(Command, Output) :-
         process_create(path(bash),
                 ['-c', Command],
@@ -14,3 +20,22 @@ whoOwns(FileName, Output):-
 	atom_concat(FirstPart, FileName, CommandStr),
 	atom_concat(CommandStr, LastPart, FinalCommandStr),
 	bash_command(FinalCommandStr, Output).
+
+server(Port) :-
+        http_server(http_dispatch, [port(Port)]).
+        
+headers:-
+        format('Access-Control-Allow-Origin: *~n'),
+        format('Content-type: application/json; charset=utf-8~n~n').
+        
+:- http_handler(/, request_response, []).
+request_response(Request) :-
+        headers,
+        http_parameters(Request, [ query(Q, []) ]),
+        response(Q, R),
+        format('~p', [R]).
+
+
+% === dataset ===
+response('who are you ?', X):-whoareyou(X).
+response(_, 'The question is unclear').
---
+++ prolog.pl
@@ -0,0 +1,23 @@
+﻿:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_error)).
+:- use_module(library(http/http_parameters)).
+
+server(Port) :-
+        http_server(http_dispatch, [port(Port)]).
+        
+headers:-
+        format('Access-Control-Allow-Origin: *~n'),
+        format('Content-type: application/json; charset=utf-8~n~n').
+        
+:- http_handler(/, request_response, []).
+request_response(Request) :-
+        headers,
+        http_parameters(Request, [ query(Q, []) ]),
+        response(Q, R),
+        format('~p', [R]).
+
+
+% === dataset ===
+response('مرحبا', 'السلام عليكم').
+response(_, 'مش فاهم').
