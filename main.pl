58ab000ca237fea1cfeb6fede9042143665d0cb3
EssamTarik <essamtarik2000@gmail.com>
2017-05-13 22:47:17 +0200

    check user ownership

--- main.pl
+++ main.pl
@@ -3,6 +3,8 @@
 :- use_module(library(http/http_error)).
 :- use_module(library(http/http_parameters)).
 
+stringEquals([], []).
+stringEquals([H|T], [H|Tt]):-stringEquals(T, Tt).
 
 bash_command(Command, Output) :-
         process_create(path(bash),
@@ -10,20 +12,6 @@ bash_command(Command, Output) :-
                 [stdout(pipe(Out))]),
         read_string(Out, _, Output),
         close(Out).
-
-
-howManyFiles(["in"|[T]], X):-getHowManyFiles(T, X).
-howMany(["files"|T], X):-howManyFiles(T, X).
-how(["many"|T], X):-howMany(T, X).
-
-whenWas([H|["modified"]], X):-getWhenWasModified(H,X).
-when(["was"|T], X):-whenWas(T, X).
-
-who(["owns"|[T]], X):-whoOwns(T, X).
-fact(["who"|T], X):- who(T, X).
-fact(["how"|T], X):- how(T, X).
-fact(["when"|T], X):- when(T, X).
-
 	
 whoareyou(X):-
 	bash_command('whoami', X).
@@ -45,19 +33,39 @@ getWhenWasModified(FileName, Output):-
 	bash_command(FinalCommandStr, Output).
 
 
-whoOwns(FileName, Output):-
-	FirstPart = "ls -l ",
-	LastPart =  "| awk '{ print $3 }'",
-	atom_concat(FirstPart, FileName, CommandStr),
-	atom_concat(CommandStr, LastPart, FinalCommandStr),
-	bash_command(FinalCommandStr, Output).
-
 getHowManyFiles(Directory, Output):-
 	FirstPart = "ls ",
 	LastPart =  "| wc -w",
 	atom_concat(FirstPart, Directory, CommandStr),
 	atom_concat(CommandStr, LastPart, FinalCommandStr),
 	bash_command(FinalCommandStr, Output).
+
+
+
+howManyFiles(["in"|[T]], X):-getHowManyFiles(T, X).
+howMany(["files"|T], X):-howManyFiles(T, X).
+how(["many"|T], X):-howMany(T, X).
+
+whenWas([H|["modified"]], X):-getWhenWasModified(H,X).
+when(["was"|T], X):-whenWas(T, X).
+
+doesOwn(User,FileName,X):-
+	atom_concat(User, '\n', UserString),
+	atom_codes(UserString, UserCodes),
+	whoOwns(FileName, RealOwner),
+	atom_codes(RealOwner, RealOwnerCodes),
+	stringEquals(UserCodes, RealOwnerCodes), X="yes".
+
+doesOwn(User,FileName,"No").
+
+does([H|["own"|[T]]], X):-doesOwn(H, T, X).
+
+who(["owns"|[T]], X):-whoOwns(T, X).
+fact(["who"|T], X):- who(T, X).
+fact(["how"|T], X):- how(T, X).
+fact(["when"|T], X):- when(T, X).
+fact(["does"|T], X):- does(T, X).
+
 
 server(Port) :-
         http_server(http_dispatch, [port(Port)]).
@@ -72,6 +80,7 @@ request_response(Request) :-
         http_parameters(Request, [ query(Q, []) ]),
         response(Q, R),
         format('~p', [R]).
+
 
 
 % === dataset ===
